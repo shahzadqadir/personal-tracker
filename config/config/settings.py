88791b6378a8bf7ff1200dc423612d630f7665b6
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2022-12-30 16:23:44 +0000

    removed sprint

--- config/config/settings.py
+++ config/config/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'track',
-    'sprint',   
+    'track',   
     # third party apps
     'crispy_forms', 
 ]
